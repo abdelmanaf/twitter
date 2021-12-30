16714e391f0db8b52a022c8fa580e2df6832bea0
abdelmanaf <ourotoumanaf6@gmail.com>
2021-12-30 14:29:18 -0500

    add side bar link component and side bar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import "./App.css";
+import {useState} from 'react'
+import BottomNavigation from '@material-ui/core/BottomNavigation'
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
+import FolderIcon from '@material-ui/icons/Folder'
+import RestoreIcon from '@material-ui/icons/Restore'
+import FavoriteIcon from '@material-ui/icons/Favorite'
+import LocationOnIcon from '@material-ui/icons/LocationOn'
+
 
 function App() {
+
   return (
-    <div className="App">
-      <h1> Hello twitter </h1>
+    <div>
+      <h1> My side bar twitter app</h1>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/SidebarLink.js
@@ -0,0 +1,13 @@
+import "sidebarLink.css";
+
+import React from 'react'
+
+function SidebarLink({ text }) {
+    return (
+        <div className="link">
+            <h2>{text}</h2>
+        </div>
+    )
+}
+
+export default SidebarLink;
