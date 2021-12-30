28db43b922a9e31fc49479cb998602b730f23bfb
abdelmanaf <ourotoumanaf6@gmail.com>
2021-12-30 16:06:26 -0500

    trnasform the more option into button, then when clicked, it opens more options

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,4 +1,6 @@
 import "./sidebar.css";
+import {useState} from "react";
+// side bar Icons
 import SidebarLink from "./SidebarLink";
 import HomeIcon from "@material-ui/icons/Home";
 import SearchIcon from "@material-ui/icons/Search";
@@ -9,9 +11,46 @@ import ListAltIcon from "@material-ui/icons/ListAlt";
 import PermIdentityIcon from "@material-ui/icons/PermIdentity";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import { Button } from "@material-ui/core";
-
+// Menu
+import { Menu } from "@material-ui/core";
+import { MenuItem } from "@material-ui/core";
+// Menu Icons
+import BookmarkBorderOutlinedIcon from '@material-ui/icons/BookmarkBorderOutlined';
+import ListAltOutlinedIcon from '@material-ui/icons/ListAltOutlined';
+import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined';
+import OfflineBoltOutlinedIcon from '@material-ui/icons/OfflineBoltOutlined';
+import PostAddOutlinedIcon from '@material-ui/icons/PostAddOutlined';
+import CallMadeOutlinedIcon from '@material-ui/icons/CallMadeOutlined';
+import BarChartOutlinedIcon from '@material-ui/icons/BarChartOutlined';
+import SettingsOutlinedIcon from '@material-ui/icons/SettingsOutlined';
+import HelpOutlineOutlinedIcon from '@material-ui/icons/HelpOutlineOutlined';
+import BrushOutlinedIcon from '@material-ui/icons/BrushOutlined';
+import AccessibilityNewOutlinedIcon from '@material-ui/icons/AccessibilityNewOutlined';
 
 function Sidebar(){
+
+    const options = [
+    { link: 'Bookmarks', icon: <BookmarkBorderOutlinedIcon fontSiz0px/> },
+    { link: 'List', icon: <ListAltOutlinedIcon/> },
+    { link: 'Topic', icon: <ChatOutlinedIcon/> },
+    { link: 'Moments', icon: <OfflineBoltOutlinedIcon/> },
+    { link: 'Newsletters', icon: <PostAddOutlinedIcon/> },
+    { link: 'Twitter Ads', icon: <CallMadeOutlinedIcon/> },
+    { link: 'Analytics', icon: <BarChartOutlinedIcon/> },
+    { link: 'Settings', icon: <SettingsOutlinedIcon/> },
+    { link: 'Help Center', icon: <HelpOutlineOutlinedIcon/> },
+    { link: 'Display', icon: <BrushOutlinedIcon/> },
+    { link: 'Keyboard shortcuts', icon: <AccessibilityNewOutlinedIcon/> },
+  ];
+
+    const [open, setOpen] = useState(false);
+    const handleClose = () => {
+        setOpen(false);
+    }
+    const handleClick = (event) => {
+        setOpen(true);
+    };
+
   return(
     <div className="sidebar">
         <SidebarLink text="Home" active={true} Icon={HomeIcon} />
@@ -21,10 +60,23 @@ function Sidebar(){
         <SidebarLink text="Bookmarks" Icon={BookmarkBorderIcon} />
         <SidebarLink text="Lists" Icon={ListAltIcon} />
         <SidebarLink text="Profile" Icon={PermIdentityIcon} />
-        <SidebarLink text="More" Icon={MoreHorizIcon}/>
+        <Button onClick={handleClick} id="moreLinks">
+            <MoreHorizIcon /> More
+        </Button>
         <Button id="tweet">
             Tweet
         </Button>
+        <Menu 
+            id="long-menu"
+            open={open}
+            onClose={handleClose}
+        >
+        </Menu>
+        {options.map((option) => (
+            <MenuItem key={option.link} onClick={handleClick}>
+            {option.icon} {option.link}
+            </MenuItem>
+        ))}
     </div>
   );
 }
